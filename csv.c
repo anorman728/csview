8add93933836275f0da07548aec3cfccefdc91c4
Andrew Norman <anorman728@gmail.com>
2023-09-22 22:11:31 -0500

    Updated csv parser to be able to change delimiter.

--- csv.c
+++ csv.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+// Note: This has been modified from the original source to fit our needs by
+// adding an delimiter option.
 
 void free_csv_line( char **parsed ) {
     char **ptr;
@@ -11,7 +15,7 @@ void free_csv_line( char **parsed ) {
     free( parsed );
 }
 
-static int count_fields( const char *line ) {
+static int count_fields( const char *line, char del) {
     const char *ptr;
     int cnt, fQuote;
 
@@ -23,15 +27,14 @@ static int count_fields( const char *line ) {
             continue;
         }
 
-        switch( *ptr ) {
-            case '\"':
-                fQuote = 1;
-                continue;
-            case ',':
-                cnt++;
-                continue;
-            default:
-                continue;
+        if ( *ptr == '\"' ) {
+            fQuote = 1;
+            continue;
+        } else if ( *ptr == del ) {
+            cnt++;
+            continue;
+        } else {
+            continue;
         }
     }
 
@@ -47,12 +50,12 @@ static int count_fields( const char *line ) {
  *  which are escaped by "double quotes", extract a NULL-terminated
  *  array of strings, one for every cell in the row.
  */
-char **parse_csv( const char *line ) {
+char **parse_csv( const char *line, char del ) {
     char **buf, **bptr, *tmp, *tptr;
     const char *ptr;
     int fieldcnt, fQuote, fEnd;
 
-    fieldcnt = count_fields( line );
+    fieldcnt = count_fields( line, del );
 
     if ( fieldcnt == -1 ) {
         return NULL;
@@ -94,38 +97,38 @@ char **parse_csv( const char *line ) {
             continue;
         }
 
-        switch( *ptr ) {
-            case '\"':
-                fQuote = 1;
-                continue;
-            case '\0':
+        if ( *ptr == '\"' ) {
+            fQuote = 1;
+            continue;
+        } else if ( *ptr == '\0' || *ptr == del ) {
+            if ( *ptr == '\0' ) {
                 fEnd = 1;
-            case ',':
-                *tptr = '\0';
-                *bptr = strdup( tmp );
+            }
 
-                if ( !*bptr ) {
-                    for ( bptr--; bptr >= buf; bptr-- ) {
-                        free( *bptr );
-                    }
-                    free( buf );
-                    free( tmp );
+            *tptr = '\0';
+            *bptr = strdup( tmp );
 
-                    return NULL;
+            if ( !*bptr ) {
+                for ( bptr--; bptr >= buf; bptr-- ) {
+                    free( *bptr );
                 }
+                free( buf );
+                free( tmp );
 
-                bptr++;
-                tptr = tmp;
+                return NULL;
+            }
 
-                if ( fEnd ) {
-                    break;
-                } else {
-                    continue;
-                }
+            bptr++;
+            tptr = tmp;
 
-            default:
-                *tptr++ = *ptr;
+            if ( fEnd ) {
+                break;
+            } else {
                 continue;
+            }
+        } else {
+            *tptr++ = *ptr;
+            continue;
         }
 
         if ( fEnd ) {
